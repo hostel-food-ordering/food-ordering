13b14e4911dd18dcdbd278ef01b398ab60eb7826
Shubham Raj <shubham282raj@gmail.com>
2024-05-28 16:12:20 +0530

    Backend: Added shop schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,10 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import router from "./routes/index_route";
 
-mongoose.connect(process.env.MONGO_DB as string);
+mongoose
+  .connect(process.env.MONGO_DB as string)
+  .then(() => console.log("Database connected successfully"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
@@ -12,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(router);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 7000;
 
