2a0fadbb5c87add9b20818c527081549bd943d92
Karan Parashar <karan@pixelos.net>
2024-05-30 12:39:43 +0530

    Backend: Call verifyToken to retrieve userId

--- backend/src/middleware/shopOwner.ts
+++ backend/src/middleware/shopOwner.ts
@@ -1,40 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import Shop from "../models/shop";
+import verifyToken from "./auth";
 
-const isOwner = async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies["auth_token"];
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized" });
-  }
+const isOwner = [
+  verifyToken,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const shopId = req.params.shop_id;
+      const userId = req.userId;
 
-  try {
-    const shopId = req.params.shop_id;
-    const userId = req.userId;
+      if (!mongoose.Types.ObjectId.isValid(shopId)) {
+        return res.status(400).send({ message: "Invalid shop ID" });
+      }
 
-    if (!mongoose.Types.ObjectId.isValid(shopId)) {
-      return res.status(400).json({ message: "Invalid shop ID" });
+      const shop = await Shop.findById(shopId);
+      if (!shop) {
+        return res.status(404).send({ message: "Shop not found" });
+      }
+
+      const isOwner = shop.ownerId.some(
+        (ownerId) => ownerId.toString() === userId
+      );
+      if (!isOwner) {
+        return res.status(403).send({
+          message: "You do not have permission to perform this action",
+        });
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something went wrong" });
     }
-
-    const shop = await Shop.findById(shopId);
-    if (!shop) {
-      return res.status(404).json({ message: "Shop not found" });
-    }
-
-    const isOwner = shop.ownerId.some(
-      (ownerId) => ownerId.toString() === userId
-    );
-    if (!isOwner) {
-      return res
-        .status(403)
-        .json({ message: "You do not have permission to perform this action" });
-    }
-
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-};
+  },
+];
 
 export default isOwner;
