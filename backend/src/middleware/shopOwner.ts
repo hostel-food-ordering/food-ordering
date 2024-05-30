2656f4830eb096e31f18d3c33cf9ca38fd3a9c8a
Shubham Raj <shubham282raj@gmail.com>
2024-05-30 15:23:58 +0530

    Backend: Fixed is owner middleware
    Check admin fixed

--- backend/src/middleware/shopOwner.ts
+++ backend/src/middleware/shopOwner.ts
@@ -1,21 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
-import Shop from "../models/shop";
+import Shop, { ShopType } from "../models/shop";
 import verifyToken from "./auth";
 import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      shop: ShopType;
+    }
+  }
+}
 
 const isOwner = [
   verifyToken,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const user = await User.findById(req.userId);
-      if (!(user && user.isAdmin)) {
-        return res.status(401).send({ message: "No Admin rights" });
-      }
-      next();
-
       if (!mongoose.Types.ObjectId.isValid(req.params.shop_id)) {
         return res.status(400).send({ message: "Invalid shop ID" });
+      }
+
+      const user = await User.findById(req.userId);
+      if (user && user.isAdmin) {
+        return next();
       }
 
       const shop = await Shop.findById(req.params.shop_id);
@@ -32,10 +39,12 @@ const isOwner = [
           message: "You do not have permission to perform this action",
         });
       }
-      next();
+
+      req.shop = shop;
+      return next();
     } catch (error) {
       console.error(error);
-      res.status(500).send({ message: "Something went wrong" });
+      return res.status(500).send({ message: "Something went wrong" });
     }
   },
 ];
