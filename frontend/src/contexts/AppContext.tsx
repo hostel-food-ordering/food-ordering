9d87e8da1274079ef3539d86bc2911fa110f14ee
Shubham Raj <shubham282raj@gmail.com>
2024-06-25 17:21:34 +0530

    Frontend: Init app context

---
+++ frontend/src/contexts/AppContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from "react";
+
+type AppContextType = {
+  isLoggedIn: boolean;
+};
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <AppContext.Provider value={{ isLoggedIn: false }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  return context as AppContextType;
+};
