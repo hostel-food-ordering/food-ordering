fbd028584ca2c89d7c5aa0e58ce95753a5038e91
Shubham Raj <shubham282raj@gmail.com>
2024-06-27 14:18:22 +0530

    Frontend: Fixed toast and added click to close

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -18,11 +18,11 @@ const Toast = ({ message, type, onClose }: ToastProps) => {
 
   const styles =
     type === "SUCCESS"
-      ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
-      : "fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md";
+      ? "fixed top-16 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
+      : "fixed top-16 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md";
 
   return (
-    <div className={styles}>
+    <div className={styles + " select-none"} onClick={onClose}>
       <div className="flex justify-center items-center">
         <span className="text-lg font-semibold">{message}</span>
       </div>
