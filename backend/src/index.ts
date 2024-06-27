e097c2c7b98319f7bef3b39590d0703ad81034f6
Shubham Raj <shubham282raj@gmail.com>
2024-06-27 19:24:04 +0530

    Backend: Serves frontend for other routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import router from "./routes/index_route";
 import path from "path";
+import { Request, Response } from "express";
 
 mongoose
   .connect(process.env.MONGO_DB as string)
@@ -21,6 +22,10 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api", router);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 7000;
 
